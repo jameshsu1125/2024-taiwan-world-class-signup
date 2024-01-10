2ecbc16d0940a981f774c2ea97fab00ebc4df3e8
jameshsu1125 <jameshsu1125@gmail.com>
2024-01-10 18:07:37 +0800

    Refactor SCHEMA object in config.ts

--- src/settings/config.ts
+++ src/settings/config.ts
@@ -6,73 +6,76 @@ export const REST_PATH = {
   test: 'todos/1',
 };
 
-export const SCHEMA = {
+export type TSchema = {
+  storeName: string;
+  userName: string;
+  userEnglishName: string;
+  userNickName: string;
+  tel: string | number;
+  email: string;
+  diageoStaff: string;
+  bartendingLevel: string;
+  bartendingName: string;
+  bartendingEnglishName: string;
+  facebookURL: string;
+  alcoholConcentration: string | number;
+  baseWineName: string;
+  baseWineEnglishName: string;
+  baseWineDosage: string | number;
+  materialName1?: string;
+  materialEnglishName1?: string;
+  materialDosage1?: string;
+  materialName2?: string | number;
+  materialEnglishName2?: string;
+  materialDosage2?: string | number;
+  materialName3?: string | number;
+  materialEnglishName3?: string;
+  materialDosage3?: string | number;
+  materialName4?: string | number;
+  materialEnglishName4?: string;
+  materialDosage4?: string | number;
+  materialName5?: string | number;
+  materialEnglishName5?: string;
+  materialDosage5?: string | number;
+  materialName6?: string | number;
+  materialEnglishName6?: string;
+  materialDosage6?: string | number;
+  garnishName?: string;
+  garnishEnglishName?: string;
+  cupsName: string;
+  cupsEnglishName: string;
+  productionMethod: string;
+  productionMethodEnglish: string;
+  introduction: string;
+  introductionEnglish: string;
+  photo: string;
+};
+
+export const SCHEMA: TSchema = {
   storeName: '',
-  user: {
-    name: '',
-    english: '',
-    nick: '',
-  },
-  phone: '',
+  userName: '',
+  userEnglishName: '',
+  userNickName: '',
+  tel: '',
   email: '',
   diageoStaff: '',
-  level: '',
-  bartending: {
-    name: '',
-    english: '',
-  },
-  facebookPostURL: '',
-  AlcoholConcentration: '',
-  baseWine: {
-    name: '',
-    english: '',
-    ml: '',
-  },
-  material1: {
-    name: '',
-    english: '',
-    ml: '',
-  },
-  material2: {
-    name: '',
-    english: '',
-    ml: '',
-  },
-  material3: {
-    name: '',
-    english: '',
-    ml: '',
-  },
-  material4: {
-    name: '',
-    english: '',
-    ml: '',
-  },
-  material5: {
-    name: '',
-    english: '',
-    ml: '',
-  },
-  material6: {
-    name: '',
-    english: '',
-    ml: '',
-  },
-  garnish: {
-    name: '',
-    english: '',
-  },
-  cups: {
-    name: '',
-    english: '',
-  },
-  productionMethod: {
-    name: '',
-    english: '',
-  },
-  introduction: {
-    name: '',
-    english: '',
-  },
-  image: '',
+  bartendingLevel: '',
+  bartendingName: '',
+  bartendingEnglishName: '',
+  facebookURL: '',
+  alcoholConcentration: '',
+  baseWineName: '',
+  baseWineEnglishName: '',
+  baseWineDosage: '',
+  materialEnglishName6: '',
+  materialDosage6: '',
+  garnishName: '',
+  garnishEnglishName: '',
+  cupsName: '',
+  cupsEnglishName: '',
+  productionMethod: '',
+  productionMethodEnglish: '',
+  introduction: '',
+  introductionEnglish: '',
+  photo: '',
 };
