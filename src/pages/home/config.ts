d3bc286b0940ffe015b0c820e0ea8889cf7affe8
jameshsu1125 <jameshsu1125@gmail.com>
2024-01-10 18:21:17 +0800

    Update function to handle edge cases
    This commit updates the function to handle edge cases and improve error handling.

--- src/pages/home/config.ts
+++ src/pages/home/config.ts
@@ -1,11 +1,33 @@
+import { TSchema } from '@/settings/config';
 import { createContext, Dispatch, SetStateAction } from 'react';
 
-export enum HomeStepType {
-  unset = 0,
-  fadeIn = 1,
-}
-export type THomeState = { step: HomeStepType };
+export const HomeData: TSchema = {
+  storeName: '',
+  userName: '',
+  userEnglishName: '',
+  userNickName: '',
+  tel: '',
+  email: '',
+  diageoStaff: '',
+  bartendingLevel: '',
+  bartendingName: '',
+  bartendingEnglishName: '',
+  facebookURL: '',
+  alcoholConcentration: '',
+  baseWineName: '',
+  baseWineEnglishName: '',
+  baseWineDosage: '',
+  cupsName: '',
+  cupsEnglishName: '',
+  productionMethod: '',
+  productionMethodEnglish: '',
+  introduction: '',
+  introductionEnglish: '',
+  photo: '',
+};
+
+export type THomeState = { data: TSchema };
 export type THomeContext = [THomeState, Dispatch<SetStateAction<THomeState>>];
 
-export const HomeState = { step: HomeStepType.unset };
+export const HomeState = { data: HomeData };
 export const HomeContext = createContext<THomeContext>([HomeState, () => {}]);
